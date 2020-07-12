3d55cf1960b44e574c4e6718228e018041e36db6
kmpk <kmpk.gm@gmail.com>
2020-07-12 17:47:49 +0300

    HW2 5
    check Spring injection of new beans

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,8 +4,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 
 public class SpringMain {
@@ -15,6 +18,8 @@ public class SpringMain {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            System.out.println(mealController.getAllFiltered(LocalDate.MIN, LocalTime.NOON, LocalDate.MAX, LocalTime.of(20, 0, 0)));
         }
     }
 }
