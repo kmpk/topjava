6f6f1bfce09d26f5245920e286e89e382b9f9098
kmpk <kmpk.gm@gmail.com>
2020-08-01 19:18:36 +0300

    HW3 fix uppercase in sql script

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -25,7 +25,7 @@ CREATE TABLE user_roles
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE MEALS
+CREATE TABLE meals
 (
   id                INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id           INTEGER                     NOT NULL,
@@ -34,4 +34,4 @@ CREATE TABLE MEALS
   calories          INTEGER                     NOT NULL,
   FOREIGN KEY (USER_ID) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_unique_user_id_date_time_idx ON MEALS (user_id,date_time);
+CREATE UNIQUE INDEX meals_unique_user_id_date_time_idx ON meals (user_id,date_time);
